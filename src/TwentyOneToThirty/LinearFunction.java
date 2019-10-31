26233a04a8d32ef9e83435791b10e631b4d17c64
Bornemann <Bornemann@PCBORNEMANN.comline.de>
2019-10-31 15:15:52 +0100

    added third package

---
+++ src/TwentyOneToThirty/LinearFunction.java
@@ -0,0 +1,26 @@
+package TwentyOneToThirty;
+
+public class LinearFunction {
+
+	static String BaueFunktion(int x1, int y1, int x2, int y2)  {
+		// Koordinaten nach x aufsteigend sortieren
+		
+		
+		
+		String result = "(";
+		
+		return result;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		int x1 = 0;
+		int y1 = 0;
+		int x2 = 1;
+		int y2 = 1;
+
+		
+	}
+
+}
